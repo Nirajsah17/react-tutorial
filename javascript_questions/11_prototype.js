833697867e789e589984779418477b15b0e3f07a
niraj <niraj@mapmyindia.com>
2024-12-24 18:07:56 +0530

    cleaned the directory structure

---
+++ javascript_questions/11_prototype.js
@@ -0,0 +1,7 @@
+/**
+ * Prototype is a Object that are attached to each function and object,
+ * It allows us to add properties or methods to objects enable inheritance and code reusebliity
+ */
+
+
+// Defining a prototype
